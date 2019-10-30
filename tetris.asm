8929fbe1af5e65a14784e5a88cb3b90c9cb59647
Bastien Wermeille <bastien.wermeille@gmail.com>
2019-10-30 09:01:06 +0100

    Create first set of functions structure

--- tetris.asm
+++ tetris.asm
@@ -1,4 +1,4 @@
-
+; SIZE 12 x 8
 
 
 ;; game state memory location
@@ -12,24 +12,29 @@
 .equ LEDS, 0x2000 ; LED address
 .equ RANDOM_NUM, 0x2010 ; Random number generator address
 .equ BUTTONS, 0x2030 ; Buttons addresses
+
 ;; tetromino type enumeration
 .equ C, 0x00 ; carre (square)
 .equ B, 0x01 ; bar-shape
 .equ T, 0x02 ; t-shape
 .equ S, 0x03 ; s-shape
 .equ L, 0x04 ; l-shape
+
 ;; GSA type
 .equ NOTHING, 0x00 ; the array location is not occupied
 .equ PLACED, 0x01 ; occupied by a nonmoving object
 .equ FALLING, 0x02 ; occupied by a falling object
+
 ;; orientation enumeration
 .equ N, 0x00 ; north
 .equ E, 0x01 ; east
 .equ So, 0x02 ; south
 .equ W, 0x03 ; west
+
 ;; Rotation enumeration
 .equ CLOCKWISE, 0 ; clockwise direction of rotation
 .equ COUNTERCLOCKWISE, 1 ; counterclockwise direction of rotation
+
 ;; Actions over tetrominos
 .equ moveL, 0x01 ; move left, horizontally
 .equ rotL, 0x02 ; rotate counterclockwise
@@ -37,32 +42,62 @@
 .equ rotR, 0x08 ; rotate clockwise
 .equ moveR, 0x10 ; move right, horizontally
 .equ moveD, 0x20 ; move down, vertically
+
 ;; Collision return ENUM
 .equ W_COL, 0x00 ; collision on the west side of tetrominoe
 .equ E_COL, 0x01 ; collision on the east side of tetrominoe
 .equ So_COL, 0x02 ; collision on the south side of tetrominoe
 .equ OVERLAP, 0x03 ; tetromino overlaps with something
 .equ NONE, 0x04 ; tetromino does not collide with anything
+
 ;; start location
 .equ START_X, 6 ; start tetromino x-axis coordinate
 .equ START_Y, 1 ; start tetromino y-axis coordinate
-;; standard limits
+
+; standard limits
 .equ X_LIMIT, 12
 .equ Y_LIMIT, 8
+
 ;; game rate of tetrominoes falling down (in terms of game loop iteration)
 .equ RATE, 5
 
+; BEGIN:main
+main:
+    jal clear_leds
+
+    addi $a0, $zero, 1
+    addi $a1, $zero, 1
+    jal set_pixel
+    
+; END:main
+
 ; BEGIN:clear_leds
 clear_leds:
+    ; TODO: LEDS from 0 to 95 set to 0
 
     ret
 ; END:clear_leds
 
 ; BEGIN:set_pixel
 set_pixel:
+    ; Params
+    ; register a0: the pixel’s x-coordinate.
+    ; register a1: the pixel’s y-coordinate.
 
     ret
 ; END:set_pixel
+
+; BEGIN:wait
+wait:
+    ; val 2^20 for real
+    ; val Smaller for simulation
+
+    ; TODO: A simple loop from  val to 0
+
+    ret
+; END:wait
+
+
 
 ; BEGIN:get_input
 get_input:
